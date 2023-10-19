3bba53993153e55af8c091776bfc94ff12720f7c
sambenko <sam.benko@gmail.com>
2023-10-19 16:42:37 +0200

    display persistency fix + largest display possible

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { pins } from "@dsboard/esp32s3_box_lite"
-import * as ds from "@devicescript/core"
-import { ST7789Driver } from "@devicescript/drivers"
-import { spi } from "@devicescript/spi"
-import { Image } from "@devicescript/graphics"
+import { pins } from "@dsboard/esp32s3_box_lite";
+import * as ds from "@devicescript/core";
+import { ST7789Driver } from "@devicescript/drivers";
+import { spi } from "@devicescript/spi";
+import { Image } from "@devicescript/graphics";
 
 spi.configure({
     mosi: pins.P6,
@@ -11,20 +11,20 @@ spi.configure({
     hz: 40_000_000,
 })
 
-pins.P45.setMode(ds.GPIOMode.OutputHigh)
+pins.P45.setMode(ds.GPIOMode.OutputHigh) // backlight set high
 
-const display = new ST7789Driver(Image.alloc(240, 320, 4), {
+const display = new ST7789Driver(Image.alloc(260, 220, 4), { 
     dc: pins.P4,
     cs: pins.P5,
     reset: pins.P48,
-    // frmctr1: 0x0e_14_ff,
-    flip: true,
+    flip: true, 
     spi: spi,
-    offX: 40,
-    offY: 53,
+    offX: 0,
+    offY: 0,  
 })
 
-await display.init()
-display.image.print("Hello world!", 3, 10)
-await display.show()
+pins.P45.setMode(ds.GPIOMode.OutputLow) // backlight set low
 
+await display.init();
+await display.image.printCenter("Hello World!", display.image.height / 2);
+await display.show();
